744f4f0f0e65cd6dfee2ed333411b8d6b07f8510
Renato Oliveira <renato.rops@gmail.com>
2018-11-08 01:04:36 -0200

    Add redux & react-redux

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,38 +1,26 @@
 import React from 'react'
 import Header from './layout/Header'
 import Main from './layout/Main'
-// import Footer from './layout/Footer'
-import { Redirect, Router, Route } from 'react-router-dom'
-import createHashHistory from 'history/createHashHistory'
+import Footer from './layout/Footer'
+// import { Redirect, Router, Route } from 'react-router-dom'
+// import createHashHistory from 'history/createHashHistory'
 
-const hashHistory = createHashHistory();
-
-const Index = () => (
-	<h1>Index</h1>
-)
-
-const About = () => (
-	<h1>About</h1>
-)
-
-const Users = () => (
-	<h1>Users</h1>
-)
+// const hashHistory = createHashHistory();
 
 const Home = () => (
    <div>
        <Header />
        <Main />
 			 
-			 	<Router history={hashHistory}>
+			 	{/* <Router history={hashHistory}>
 					 <div>
 						 	<Route path="/" exact component={Index} />
       				<Route path="/about/" component={About} />
       				<Route path="/users/" component={Users} />
 					 </div>
-				</Router>
+				</Router> */}
 			 
-			 {/* <Footer /> */}
+			 <Footer />
    </div>
 )
     
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import "./assets/styles/_all.scss"
-
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {combineReducers, createStore } from 'redux'
+import {Provider} from 'react-redux'
+import './assets/styles/_all.scss'
 import Home from './Home'
 
+const reducers = combineReducers({
+	field: ()=>({value: 'Opa'})
+})
+
+const store = createStore(
+	reducers, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <Home />,
+  <Provider store={store}>
+		<Home />
+	</Provider>,
   document.getElementById('app')
 );
 
